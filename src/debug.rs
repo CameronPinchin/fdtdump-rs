355817fe82851237c11c3fd05d2a7123826dc658
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2026-02-11 21:08:10 -0500

    expansion

--- src/debug.rs
+++ src/debug.rs
@@ -16,4 +16,27 @@ pub fn dump_blob( file: &str ) {
     println!("This is a device tree representation of a {}:", fdt.root().model());
     println!("...which is compatible with atleast: {}", fdt.root().compatible().first());
     println!("...which has {} CPUs", fdt.cpus().count());
+    println!(
+      "...and has atleast one memory location at: {:#X}\n",
+      fdt.memory.regions().next().unwrap().starting_address as usize
+   );
+
+   let chosen = fdt.chosen();
+   if let Some(bootargs) = chosen.bootargs() {
+      println!("The bootargs are: {:?}", bootargs);
+   }
+
+   if let Some(stdout) = chosen.stdout() {
+      println!("It would write stdout to: {}", stdout.name);
+   }
+
+   let soc = fdt.find_node("/soc");
+   println!("Does it have a '/soc' node? {}", if soc.is_some() { "yes" } else { "no" });
+   if let Some(soc) = soc {
+      println!("...and it has the following children:");
+      for child in soc.children() {
+         println!("  {}", child.name);
+      }
+   }
+
  }
