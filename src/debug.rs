b55d526ea0828c7533a01820dae12fc974164590
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2026-02-11 20:17:22 -0500

    dump_blob

---
+++ src/debug.rs
@@ -0,0 +1,16 @@
+/* @author Cameron Pinchin 
+ * @date Feb 11 2026 
+ * @brief This handles debug functionality when '-d, --debug' is passed. 
+ * May include helper methods.
+ */
+
+use::fdt;
+
+mod error;
+
+ fn dump_blob( &str file, bool debug ) {
+    let fdt = Fdt::new(error::TEST_PATH_EXTENSION).unwrap();
+    println!("This is a device tree representation of a {}:", fdt.root().model());
+    println!("...which is compatible with atleast: {}", fdt.root().compatible().first());
+    println!("...which has {} CPUs", fdt.cpus().count());
+ }
