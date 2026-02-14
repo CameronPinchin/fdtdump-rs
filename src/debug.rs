3507c5dd958624fcf9c505c1d8960196329eec2d
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2026-02-14 16:35:33 -0500

    dump_blob_test

--- src/debug.rs
+++ src/debug.rs
@@ -6,16 +6,65 @@
 
 use fdt::Fdt;
 use std::env; 
+use std::fs;
 
 use crate::error;
 
-fn grab_file_directory() {
+fn grab_file_directory() -> &str {
    match env::current_dir() {
-      Ok(path) => println!("current working path: {}", path.display()),
+      Ok(path) => return path,
       Err(e) => eprintln!("Error getting working path: {}", e),
 
    }
 }
+
+pub fn dump_blob_test( file: &str ) {
+   let mut curr_path = env::current_dir().expect("Failed to get current working directory.");
+   curr_path.push(file);
+
+   let file_bytes = fs::read(&curr_path)
+      .expect(&format!("Could not find {} in your current directory.", curr_path.display()));
+
+   // static MY_FDT: &[u8] = include_bytes!( concat!(env!("CARGO_MANIFEST_DIR"), "/_test/testfile.dtb") );
+
+    let fdt = Fdt::new(&file_bytes).unwrap();
+    println!("This is a device tree representation of a {}:", fdt.root().model());
+    println!("...which is compatible with atleast: {}", fdt.root().compatible().first());
+    println!("...which has {} CPUs", fdt.cpus().count());
+    println!(
+      "...and has atleast one memory location at: {:#X}\n",
+      fdt.memory().regions().next().unwrap().starting_address as usize
+   );
+
+   let chosen = fdt.chosen();
+   if let Some(bootargs) = chosen.bootargs() {
+      println!("The bootargs are: {:?}", bootargs);
+   }
+
+   if let Some(stdout) = chosen.stdout() {
+      println!("It would write stdout to: {}", stdout.name);
+   }
+
+   let soc = fdt.find_node("/soc");
+   println!("Does it have a '/soc' node? {}", if soc.is_some() { "yes" } else { "no" });
+   if let Some(soc) = soc {
+      println!("...and it has the following children:");
+      for child in soc.children() {
+         println!("  {}", child.name);
+      }
+   }
+
+   println!("\nFind all nodes for '/':");
+   for node in fdt.find_all_nodes("/") {
+      println!("{}", node.name);
+   }
+
+   println!("Find all nodes for '/soc/virtio':");
+   for node in fdt.find_all_nodes("/soc/virtio") {
+      println!("{}", node.name);
+   }
+
+ }
 
 /* @brief Input a file path string to parse the dtb file  
  *
